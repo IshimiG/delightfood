4e3f4ad84a7cac831b6abbb2db112fa10bdab79d
isma-MR <ismael.monleon73@gmail.com>
2025-02-19 09:39:58 +0100

    Update BD.sql

--- Tercera Entrega - Diana/BD.sql
+++ Tercera Entrega - Diana/BD.sql
@@ -11,11 +11,11 @@ CREATE TABLE IF NOT EXISTS Mesa (
 
 DROP TAbLE IF EXISTS Reserva;
 CREATE TABLE Reserva(
-    id_reserva (VARCHAR, 20) AUTO_INCREMENT PRIMARY KEY,
-    num_comensales (INT) NOT NULL,
-    notas (VARCHAR, 255),
-    fecha_reserva (DATE) NOT NULL,
-    id_mesa (VARCHAR, 20) NOT NULL,
+    id_reserva INT AUTO_INCREMENT PRIMARY KEY,
+    num_comensales INT NOT NULL,
+    notas VARCHAR (255),
+    fecha_reserva DATE NOT NULL,
+    id_mesa INT NOT NULL,
     FOREIGN KEY (id_mesa) REFERENCES Mesa(id_mesa)
 );
 
@@ -25,15 +25,16 @@ CREATE TABLE Pedido (
     precio_total DECIMAL(10, 2) NOT NULL,
     fecha_pedido DATE NOT NULL,
     entregado BOOLEAN NOT NULL,
+    id_mesa INT NOT NULL,
     FOREIGN KEY  (id_mesa) REFERENCES Mesa(id_mesa)
 );
 
-DROP TAbLE IF EXISTS Item_pedido;
+DROP TABLE IF EXISTS Item_pedido;
 CREATE TABLE Item_pedido (
     id_item_pedido INT AUTO_INCREMENT PRIMARY KEY,
     cantidad INT NOT NULL,
     id_pedido INT NOT NULL,
-    FOREIGN KEY (id_pedido) REFERENCES Pedido(id_pedido);
+    FOREIGN KEY (id_pedido) REFERENCES Pedido(id_pedido)
 );
 
 DROP TABLE IF EXISTS Plato;
@@ -43,7 +44,7 @@ CREATE TABLE Plato (
     code_img VARCHAR(50) NOT NULL,
     tiempo_estimado INT NOT NULL, -- En segundos
     precio DOUBLE NOT NULL,
-    categoria VARCHAR(50) NOT NULL
+    categoria VARCHAR(50) NOT NULL,
     id_item_pedido INT NOT NULL,
     FOREIGN KEY (id_item_pedido) REFERENCES Item_Pedido(id_item_pedido)
 );

27856ee38abad5bafcd6f1ad391ee9eaa689535d
Enrique Gracia <enriquegraciasolis@gmail.com>
2025-02-19 21:25:25 +0100

    Actualizado mockup y BD

--- Tercera Entrega - Diana/BD.sql
+++ Tercera Entrega - Diana/BD.sql
@@ -48,3 +48,51 @@ CREATE TABLE Plato (
     id_item_pedido INT NOT NULL,
     FOREIGN KEY (id_item_pedido) REFERENCES Item_Pedido(id_item_pedido)
 );
+
+INSERT INTO Mesa (token, n_comensales, ubicación) VALUES
+('', 4, 'Terraza'),
+('', 2, 'Interior'),
+('', 6, 'Patio'),
+('', 4, 'Terraza'),
+('', 2, 'Interrior'),
+('', 6, 'patio');
+
+INSERT INTO Reserva (num_comensales, notas, fecha_reserva, id_mesa) VALUES
+(4, 'Cumpleaños', '2025-02-20', 1),
+(2, 'Cena romantica', '2025-02-21', 2),
+(6, 'Reunión familiar', '2025-02-22', 3),
+(4, 'Cena de negocios', '2025-02-23', 4),
+(2, 'Aniversario', '2025-02-24', 5),
+(6, 'Fiesta sorpresa', '2025-02-25', 6);
+
+INSERT INTO Pedido (precio_total, fecha_pedido, entregado, id_mesa) VALUES
+(45.50, '2025-02-20', FALSE, 1),
+(22.30, '2025-02-21', TRUE, 2),
+(78.90, '2025-02-22', FALSE, 3),
+(55.60, '2025-02-23', TRUE, 4),
+(33.20, '2025-02-24', FALSE, 5),
+(88.70, '2025-02-25', TRUE, 6);
+
+INSERT INTO Item_pedido (cantidad, id_pedido) VALUES
+(2, 1),
+(1, 1),
+(3, 2),
+(1, 3),
+(2, 3),
+(1, 4),
+(2, 4),
+(3, 5),
+(1, 6),
+(2, 6);
+
+INSERT INTO Plato (label, code_img, tiempo_estimado, precio, categoria, id_item_pedido) VALUES
+('Sushi Nigiri', 'nigiri1', 600, 9.50, 'Nigiri', 1),
+('Sushi Maki', 'maki1', 600, 8.00, 'Maki', 2),
+('Sushi Temaki', 'temaki1', 600, 10.00, 'Temaki', 3),
+('Sushi California Roll', 'roll1', 600, 12.00, 'Roll', 4),
+('Sashimi de Salmón', 'sashimi1', 600, 15.00, 'Sashimi', 5),
+('Sashimi de Atún', 'sashimi2', 600, 16.00, 'Sashimi', 6),
+('Tartar de Atún', 'tartar1', 900, 18.00, 'Tartar', 7),
+('Tempura de Camarón', 'tempura1', 1200, 12.00, 'Tempura', 8),
+('Edamame', 'edamame', 300, 5.00, 'Entrante', 9),
+('Sopa Miso', 'sopa1', 600, 4.50, 'Sopa', 10);
